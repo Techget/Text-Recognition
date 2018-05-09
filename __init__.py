7a55c26358bc007bae2e7fd78726af1b5b61ea93
Clinton Feng <clintonfeng@gmail.com>
2018-05-09 15:34:04 +1000

    bugfix

--- __init__.py
+++ __init__.py
@@ -20,10 +20,10 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # Need to run SWT algorithm to get rid of non text
-    PIL_img = Image.open(args.image)
-    PIL_no_text_img = pf.swt(PIL_img, output_type=pf.SWT_OUTPUT_ORIGINAL_BOXES)
-    to_extract_img = PIL_to_cv_img(PIL_no_text_img)
-    #to_extract_img = cv2.imread(args.image, 1)
+    #PIL_img = Image.open(args.image)
+    #PIL_no_text_img = pf.swt(PIL_img, output_type=pf.SWT_OUTPUT_ORIGINAL_BOXES)
+    #to_extract_img = PIL_to_cv_img(PIL_no_text_img)
+    to_extract_img = cv2.imread(args.image, 1)
 
     if DEBUG:
         cv2.imshow('img', to_extract_img)
